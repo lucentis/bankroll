6a5f5593d88f9ebb5d96e089e3c16a468f9a1171
lucentis <lucentis.dev@gmail.com>
2026-03-27 09:34:59 +0100

    made action validation

--- src/components/hand/HandCreation.vue
+++ src/components/hand/HandCreation.vue
@@ -1,117 +1,13 @@
 <script setup lang="ts">
 import { bankrollStore } from '@/store/bankroll';
-import type { Card, PlayerAction, Position, Player } from '@/types/hand';
-import { computed, reactive } from 'vue';
+import type { Card, PlayerAction, Position, Player, Action, Street } from '@/types/hand';
+import { computed, reactive, ref } from 'vue';
 import CardPicker from './CardPicker.vue';
 import { Separator } from '../ui/separator';
 import { Card as UiCard, CardContent, CardHeader, CardTitle, CardFooter } from '../ui/card';
 import { Button } from '../ui/button';
 import { ArrowLeft, Plus, Trash2 } from 'lucide-vue-next';
-import type { Action } from '@/types/hand';
 import { Input } from '../ui/input';
-
-
-//----------------------------------------------------------------------------
-// Setup
-// ---------------------------------------------------------------------------
-const DEFAULT_STACK = 100
-const POSITIONS: Position[] = ['UTG', 'UTG+1', 'MP', 'HJ', 'CO', 'BTN', 'SB', 'BB']
-const ACTIONS: Action[] = ['fold', 'check', 'call', 'raise']
-
-const session = bankrollStore.sessions.find(s => s.id === bankrollStore.activeSessionId)
-const blinds = session?.stakes?.split('/')
-
-const setupState = reactive({
-    handStatus: 'setup',
-    holeCards: [] as Card[],
-    position: 'BTN',
-    errors: {} as Record<string, string>,
-    streets: [
-        { street: 'preflop', cards: [], actions: [] as PlayerAction[], currentBet: 0, playersToAct: [], contributed: {} },
-        { street: 'flop',    cards: [], actions: [] as PlayerAction[], currentBet: 0, playersToAct: [], contributed: {} },
-        { street: 'turn',    cards: [], actions: [] as PlayerAction[], currentBet: 0, playersToAct: [], contributed: {} },
-        { street: 'river',   cards: [], actions: [] as PlayerAction[], currentBet: 0, playersToAct: [], contributed: {} },
-    ],
-    players: [
-        { id: 'hero', name: 'Hero', stack: DEFAULT_STACK, position: 'BTN' },
-    ],
-    board: [] as Card[],
-    smallBlind: Number(blinds?.[0]) || 10,
-    bigBlind: Number(blinds?.[1]) || 20,
-    pot: 0
-})
-
-const addPlayer = () => {
-  const id = `villain_${setupState.players.length}`
-
-  const usedPositions = new Set(setupState.players.map(p => p.position))
-
-  const availablePosition = POSITIONS.find(p => !usedPositions.has(p))
-
-  if (!availablePosition) return // max joueurs atteint
-
-  setupState.players.push({
-    id,
-    name: `Villain ${setupState.players.length}`,
-    stack: DEFAULT_STACK,
-    position: availablePosition,
-  })
-}
-
-const removePlayer = (id: string) => {
-  if (id === 'hero') return // interdit
-
-  setupState.players = setupState.players.filter(p => p.id !== id)
-}
-
-const validatePlayers = () => {
-    // Préparer la liste des joueurs présents
-    const players = setupState.players
-    const contributed: Record<string, number> = {}
-
-    // Identifier SB et BB
-    const sbPlayer = players.find(p => p.position === 'SB')
-    const bbPlayer = players.find(p => p.position === 'BB')
-
-    // Retirer blindes des joueurs si présents
-    if (sbPlayer) {
-        const sb = setupState.smallBlind
-        sbPlayer.stack -= sb
-        contributed[sbPlayer.id] = sb
-        setupState.pot += sb
-    } else {
-        setupState.pot += setupState.smallBlind
-    }
-
-    if (bbPlayer) {
-        const bb = setupState.bigBlind
-        bbPlayer.stack -= bb
-        contributed[bbPlayer.id] = bb
-        setupState.pot += bb
-    } else {
-        setupState.pot += setupState.bigBlind
-    }
-
-    const playersToAct: Player[] = []
-
-    POSITIONS.forEach(pos => {
-        const p = players.find(pl => pl.position === pos)
-        if (p) {
-            playersToAct.push(p as Player)
-        }
-    })
-
-    console.log(players, playersToAct);
-    
-    //pot
-
-    //status
-}
-
-//----------------------------------------------------------------------------
-// Streets
-// ---------------------------------------------------------------------------
-
 
 //----------------------------------------------------------------------------
 // Utils
@@ -140,7 +36,169 @@ const getActionByPlayer = (streetIdx: number, playerId: string) =>
   setupState.streets[streetIdx].actions.find(a => a.playerId === playerId)
 const getActionIndex = (streetIdx: number, playerId: string) =>
   setupState.streets[streetIdx].actions.findIndex(a => a.playerId === playerId)
+const getStreetById = (streetId: Street) => 
+    setupState.streets.find(street => street.street == streetId)
 
+//----------------------------------------------------------------------------
+// Setup
+//----------------------------------------------------------------------------
+const DEFAULT_STACK = 100
+const POSITIONS: Position[] = ['UTG', 'UTG+1', 'MP', 'HJ', 'CO', 'BTN', 'SB', 'BB']
+const ACTION: Action[] = [ 'fold', 'call', 'check', 'raise']
+
+const session = bankrollStore.sessions.find(s => s.id === bankrollStore.activeSessionId)
+const blinds = session?.stakes?.split('/')
+
+const setupState = reactive({
+  handStatus: 'setup' as 'setup' | 'playing' | 'result',
+
+  holeCards: [] as Card[],
+  position: 'BTN' as Position,
+  errors: {} as Record<string, string>,
+
+  streets: [
+    { street: 'preflop', cards: [], actions: [] as PlayerAction[], currentBet: 0, playersToAct: [] as Player[], contributed: {} as Record<string, number> },
+    { street: 'flop', cards: [], actions: [], currentBet: 0, playersToAct: [], contributed: {} },
+    { street: 'turn', cards: [], actions: [], currentBet: 0, playersToAct: [], contributed: {} },
+    { street: 'river', cards: [], actions: [], currentBet: 0, playersToAct: [], contributed: {} },
+  ],
+
+  players: [
+    { id: 'hero', name: 'Hero', stack: DEFAULT_STACK, position: 'BTN' as Position },
+  ],
+
+  smallBlind: Number(blinds?.[0]) || 10,
+  bigBlind: Number(blinds?.[1]) || 20,
+
+  pot: 0,
+  board: []
+})
+
+//----------------------------------------------------------------------------
+// Players
+//----------------------------------------------------------------------------
+const addPlayer = () => {
+  const id = `villain_${setupState.players.length}`
+  const used = new Set(setupState.players.map(p => p.position))
+  const pos = POSITIONS.find(p => !used.has(p))
+  if (!pos) return
+
+  setupState.players.push({
+    id,
+    name: `Villain ${setupState.players.length}`,
+    stack: DEFAULT_STACK,
+    position: pos,
+  })
+}
+
+const removePlayer = (id: string) => {
+  if (id === 'hero') return
+  setupState.players = setupState.players.filter(p => p.id !== id)
+}
+
+//----------------------------------------------------------------------------
+// INIT HAND
+//----------------------------------------------------------------------------
+const validatePlayers = () => {
+  if (setupState.handStatus !== 'setup') return
+
+  const players = setupState.players
+  const contributed: Record<string, number> = {}
+
+  setupState.pot = 0
+
+  const sbPlayer = players.find(p => p.position === 'SB')
+  const bbPlayer = players.find(p => p.position === 'BB')
+
+  if (sbPlayer) {
+    sbPlayer.stack -= setupState.smallBlind
+    contributed[sbPlayer.id] = setupState.smallBlind
+    setupState.pot += setupState.smallBlind
+  } else {
+    setupState.pot += setupState.smallBlind
+  }
+
+  if (bbPlayer) {
+    bbPlayer.stack -= setupState.bigBlind
+    contributed[bbPlayer.id] = setupState.bigBlind
+    setupState.pot += setupState.bigBlind
+  } else {
+    setupState.pot += setupState.bigBlind
+  }
+
+  // init contributed à 0
+  players.forEach(p => {
+    if (!contributed[p.id]) contributed[p.id] = 0
+  })
+
+  const playersToAct: Player[] = []
+  POSITIONS.forEach(pos => {
+    const p = players.find(pl => pl.position === pos)
+    if (p) playersToAct.push(p as Player)
+  })
+
+  setupState.streets[0] = {
+    street: 'preflop',
+    cards: [],
+    actions: [],
+    playersToAct: [...playersToAct],
+    currentBet: setupState.bigBlind,
+    contributed: { ...contributed }
+  }
+
+  setupState.handStatus = 'playing'
+}
+
+//----------------------------------------------------------------------------
+// GAME LOGIC
+//----------------------------------------------------------------------------
+const currentStreetId = ref<Street>('preflop')
+const currentStreet = computed(() => getStreetById(currentStreetId.value))
+const currentAction = computed(() => currentStreet.value?.actions[currentStreet.value.actions.length -1])
+const currentPlayer = computed(() => currentStreet.value?.playersToAct[0])
+
+const toCall = computed(() => {
+    if (!currentPlayer.value) return 0
+
+    const id = currentPlayer.value.id
+    const contributed = currentStreet.value?.contributed[id] || 0
+
+    return currentStreet.value?.currentBet! - contributed
+})
+
+const setAction = (action: Action) => {
+    if(!currentAction.value || currentAction.value.playerId !== currentPlayer.value?.id) {
+        currentStreet.value?.actions.push({
+            playerId: currentPlayer.value?.id!,
+            action
+        })
+    } else if(currentAction.value.playerId == currentPlayer.value?.id) {
+        currentAction.value.action = action
+    }
+}
+
+const setAmount = (amount: number) => {
+    if (!currentAction.value) return
+    currentAction.value.amount = amount
+}
+
+const validateAction = () => {
+    //fold
+    if (currentAction.value?.action == 'fold') {
+        currentStreet.value?.playersToAct.shift()
+    }
+
+    //check
+    if (currentAction.value?.action == 'check') {
+        const first = currentStreet.value?.playersToAct.shift()
+        currentStreet.value?.playersToAct.push(first!)
+    }
+
+    // call
+
+    //raise
+}
+    
 </script>
 
 <template>
@@ -203,21 +261,21 @@ const getActionIndex = (streetIdx: number, playerId: string) =>
 
                     <!-- Hero position -->
                     <div class="space-y-1.5">
-                    <label class="text-xs font-medium text-stone-500">Position du héro</label>
-                    <div class="flex flex-wrap gap-1.5">
-                        <button
-                            v-for="pos in POSITIONS"
-                            :key="pos"
-                            type="button"
-                            class="text-xs px-3 py-1.5 rounded-full border font-mono font-medium transition-colors duration-100"
-                            :class="setupState.position === pos
-                                ? 'bg-primary/70 text-primary-foreground border-primary/10'
-                                : 'bg-white text-stone-500 border-stone-200 hover:border-stone-300'"
-                            @click="setupState.position = pos; setupState.players[0].position = pos"
-                        >
-                        {{ pos }}
-                        </button>
-                    </div>
+                        <label class="text-xs font-medium text-stone-500">Position du héro</label>
+                        <div class="flex flex-wrap gap-1.5">
+                            <button
+                                v-for="pos in POSITIONS"
+                                :key="pos"
+                                type="button"
+                                class="text-xs px-3 py-1.5 rounded-full border font-mono font-medium transition-colors duration-100"
+                                :class="setupState.position === pos
+                                    ? 'bg-primary/70 text-primary-foreground border-primary/10'
+                                    : 'bg-white text-stone-500 border-stone-200 hover:border-stone-300'"
+                                @click="setupState.position = pos; setupState.players[0].position = pos"
+                            >
+                            {{ pos }}
+                            </button>
+                        </div>
                     </div>
 
                 </CardContent>
@@ -294,14 +352,87 @@ const getActionIndex = (streetIdx: number, playerId: string) =>
         <div class="space-y-2">
             <UiCard class="border-stone-200 shadow-sm">
                 <CardHeader>
-                    <CardTitle class="text-sm font-medium text-stone-500">Préflop</CardTitle>
+                    <CardTitle class="font-medium text-stone-500 flex justify-between">
+                        <span> Préflop</span>
+                        <span class="text-xl">Pot: {{ setupState.pot }}€</span>
+                    </CardTitle>
                 </CardHeader>
 
-                <CardContent></CardContent>
+                <CardContent>
+
+                    <div class="space-y-1.5">
+                        <h3 class="text-xs font-medium text-stone-500 flex justify-between">
+                            <span>{{ currentPlayer?.name }}</span>
+                        </h3>
+
+                        <div class="flex flex-wrap gap-1.5">
+                            <button
+                                type="button"
+                                class="text-xs px-3 py-1.5 rounded-full border font-mono font-medium transition-colors duration- hover:bg-slate-200"
+                                :class="currentAction?.action === 'fold'
+                                    ? 'bg-primary/70 text-primary-foreground border-primary/10'
+                                    : 'bg-white text-stone-500 border-stone-200 hover:border-stone-300'"
+                                @click="setAction('fold')"
+                            >
+                                <span>Fold</span>
+                            </button>
+
+                            <button
+                                type="button"
+                                class="text-xs px-3 py-1.5 rounded-full border font-mono font-medium transition-colors duration- hover:bg-slate-200"
+                                :class="currentAction?.action === 'fold'
+                                    ? 'bg-primary/70 text-primary-foreground border-primary/10'
+                                    : 'bg-white text-stone-500 border-stone-200 hover:border-stone-300'"
+                                @click="setAction('fold')"
+                            >
+                                <span>Fold</span>
+                            </button>
+
+                            <Input v-if="currentAction?.action == 'raise'" type="number" @update:model-value="setAmount"/>
+
+                            <Button @click="validateAction">Valider</Button>
+                        </div>
+                    </div>
+                    <!-- <div v-if="setupState.handStatus === 'playing'">
+
+                        
+
+                        <div v-if="currentPlayer" class="space-y-2">
+                            <div class="font-semibold">
+                                {{ currentPlayer.name }} ({{ currentPlayer.position }})
+                            </div>
+
+                            <div class="text-xs text-stone-500">
+                                À payer: {{ toCall }}€
+                            </div>
+
+                            <div class="flex gap-2">
+                                <button @click="applyAction('fold')">Fold</button>
+
+                                <button v-if="toCall === 0" @click="applyAction('check')">Check</button>
+
+                                <button v-else @click="applyAction('call')">Call</button>
+
+                                <button @click="applyAction('raise', currentStreet.currentBet * 2)">
+                                Raise x2
+                                </button>
+                            </div>
+                        </div>
+
+                    </div> -->
+
+                    <!-- <div v-if="setupState.handStatus === 'result'">
+                        <div class="text-green-600 font-bold">
+                        Main terminée
+                        </div>
+                    </div> -->
+                </CardContent>
             </UiCard>
             
         </div>
     </div>
-
+    <pre>players: {{  currentPlayer }}</pre>
+    <pre>action: {{  currentAction }}</pre>
+    <pre>street: {{  currentStreet }}</pre>
     <pre>{{ setupState }}</pre>
 </template>
